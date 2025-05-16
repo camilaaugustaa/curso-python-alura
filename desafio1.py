91afb3b0ec16860ade116be858513e9905934c60
camilaaugustaa <camilaaugustasnt@outlook.com>
2025-05-16 15:51:47 -0300

    escolhas aleatorias

--- desafio1.py
+++ desafio1.py
@@ -9,3 +9,7 @@ notas = [8.5, 9, 6.5]
 plt.bar(x = estudantes, height = notas)#x é o eixo x e height é o eixo y. o eixo x esta na horizontal e o eixo y esta na vertical
 plt.show()#essa função mostra o gráfico
 
+from random import choice #essa biblioteca é para fazer escolhas aleatórias
+estudantes_2 = ["Helena", "Higor", "Juliana"]
+estudantes = choice(estudantes_2)
+print(estudantes)
